5374dc80da84a5752cc0b370bdcca8b59bb46c9e
ikaliuzh <ikaliuzh@gmail.com>
2021-10-31 12:03:57 +0200

    fixes

--- src/client.rs
+++ src/client.rs
@@ -6,12 +6,12 @@ use std::str::from_utf8;
 fn main() {
 	match TcpStream::connect("localhost:3333") {
 		Ok(mut stream) => {
-			loop {
+			println!("Successfully connected to server in port 3333");
+            loop {
 				let mut msg_input = String::new();
 				io::stdin().read_line(&mut msg_input).expect("Failed to read line!");
 				msg_input = msg_input.trim().to_owned();
 				let msg = msg_input.as_bytes();
-				println!("Successfully connected to server in port 3333");
 				stream.write(msg).unwrap();
 				println!("Sent message \"{}\", awaiting reply...", msg_input.trim());
 
